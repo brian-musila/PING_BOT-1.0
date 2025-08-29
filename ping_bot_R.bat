6b6d7149c8619cf2504417fc5b0a65faf85e7a74
Dev.Brian Musila <100756406+brian-musila@users.noreply.github.com>
2025-08-29 15:13:46 +0300

    Update ping_bot_R.bat

--- ping_bot_R.bat
+++ ping_bot_R.bat
@@ -1,4 +1,5 @@
 @echo off
-cd /d "C:\Users\Brian\Documents\PROJECT REM.ME\ping_bot.py"
+cd /d "C:\Users\Brian\Documents\PROJECT REM.ME\ping_bot.py" -- replace with actual path where youve installed the ping-bot
 start /min pythonw ping_bot.py
 exit
+
